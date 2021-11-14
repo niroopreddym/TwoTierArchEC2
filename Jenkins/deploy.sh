9befb9fc7dbb59b910bc353886b0433e247117d7
Niroop Reddy Maneti <Niroop.Maneti@carrier.com>
2021-11-14 16:11:16 +0530

    shell changes 2

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -45,45 +45,34 @@ cd "${GITHUB_WORKSPACE}"/infrastructure
 aws cloudformation package \
     --template-file vpcstack.yml \
     --s3-bucket "${ARTIFACT_NAME}" \
-    --output-template-file release/vpcstack_release.yaml
+    --output-template-file vpcstack_release.yaml
 
 aws cloudformation package \
     --template-file loadbalancer.yml \
     --s3-bucket "${ARTIFACT_NAME}" \
-    --output-template-file release/loadbalancer_release.yaml
+    --output-template-file loadbalancer_release.yaml
 
 aws cloudformation package \
     --template-file apistack.yml \
     --s3-bucket "${ARTIFACT_NAME}" \
-    --output-template-file release/apistack_release.yaml
+    --output-template-file apistack_release.yaml
 
 aws cloudformation package \
 --template-file masterstack.yml \
 --s3-bucket "${ARTIFACT_NAME}" \
---output-template-file release/masterstack_release.yaml
+--output-template-file masterstack_release.yaml
 
 cd "${GITHUB_WORKSPACE}"
 
-aws s3 cp release/vpcstack_release.yaml s3://"${ARTIFACT_NAME}"/release/vpcstack_release.yaml
-aws s3 cp release/loadbalancer_release.yaml s3://"${ARTIFACT_NAME}"/release/loadbalancer_release.yaml
-aws s3 cp release/apistack_release.yaml s3://"${ARTIFACT_NAME}"/release/apistack_release.yaml
+aws s3 cp release/vpcstack_release.yaml s3://"${ARTIFACT_NAME}"/vpcstack_release.yaml
+aws s3 cp release/loadbalancer_release.yaml s3://"${ARTIFACT_NAME}"/loadbalancer_release.yaml
+aws s3 cp release/apistack_release.yaml s3://"${ARTIFACT_NAME}"/apistack_release.yaml
 
 ##
 # Deploy infrastructure stack.
 ##
 aws cloudformation deploy \
-    --template-file "${GITHUB_WORKSPACE}"/infrastructure/release/s3updates-stack_release.yaml \
-    --stack-name "${TARGET_ENVIRONMENT}"-"${PREFIX}"-event-stack\
-	--s3-bucket "${ARTIFACT_NAME}" \
-	--capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM \
-	--no-fail-on-empty-changeset \
-	--parameter-overrides \
-	Environment="${TARGET_ENVIRONMENT}" \
-    Prefix="${PREFIX}" \
-    BucketName="${SNSBUCKETNAME}" \
-
-aws cloudformation deploy \
-    --template-file "${GITHUB_WORKSPACE}"/infrastructure/release/s3updates-stack_release.yaml \
+    --template-file "${GITHUB_WORKSPACE}"/infrastructure/aws-stacks/masterstack_release.yaml \
     --stack-name "${TARGET_ENVIRONMENT}"-"${PREFIX}"-master \
 	--s3-bucket "${ARTIFACT_NAME}" \
 	--capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM \
